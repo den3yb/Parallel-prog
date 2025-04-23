4c0309773d2d2311ad96cfeeaa2dae8b3944b3f9
den3yb <den3yb@gmail.com>
2025-04-23 14:59:24 +0400

    Финальные изменения

--- graph.py
+++ graph.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 
 def average_plot_from_files(filenames, label):
     """
-    Строит график по нескольким файлам со статистикой, усредняя значения и добавляет метку.
+    Строит график по нескольким файлам со статистикой, усредняя значения
     """
     all_x = []
     all_y = []
 
-    # Читаем данные из всех файлов
     for filename in filenames:
         x = []
         y = []
@@ -23,29 +22,24 @@ def average_plot_from_files(filenames, label):
         all_x.append(x)
         all_y.append(y)
 
-    # Находим уникальные значения x, чтобы по ним усреднять
     unique_x = sorted(list(set().union(*all_x)))
 
-    # Усредняем значения y для каждого x
     average_y = []
     for x_val in unique_x:
         y_values_for_x = []
         for i in range(len(filenames)):
-            if len(all_x[i]) == 0: # Проверяем, есть ли данные в all_x[i]
-                continue # Если нет, переходим к следующему файлу
+            if len(all_x[i]) == 0:
+                continue
+            index = all_x[i].index(x_val)
+            y_values_for_x.append(all_y[i][index])
 
-            try:
-                index = all_x[i].index(x_val)
-                y_values_for_x.append(all_y[i][index])
-            except ValueError:
-                pass # x_val отсутствует в текущем файле
 
         if y_values_for_x:
             average_y.append(sum(y_values_for_x) / len(y_values_for_x))
         else:
             average_y.append(None)
 
-    # Убираем значения None, чтобы matplotlib не ругался
+    
     x_filtered = []
     y_filtered = []
     for i in range(len(unique_x)):
@@ -56,7 +50,7 @@ def average_plot_from_files(filenames, label):
     return x_filtered, y_filtered, label
 
 if __name__ == '__main__':
-    plt.figure(figsize=(10, 6))  # Увеличиваем размер графика для лучшей читаемости
+    plt.figure(figsize=(10, 6)) 
 
     # Данные для 8 потоков
     filenames_8 = [f'Parallel-prog\\matrix_out\\matrix_statistic8{i}.txt' for i in range(1, 6)]
@@ -77,5 +71,5 @@ if __name__ == '__main__':
     plt.ylabel('Среднее время выполнения (секунды)')
     plt.title('Усредненное время выполнения умножения матриц для разного количества потоков')
     plt.grid(True)
-    plt.legend()  # Добавляем легенду, чтобы различать графики
+    plt.legend()
     plt.show()
