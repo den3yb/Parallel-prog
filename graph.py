5fbac4685850b86f7bece9e93c2465f284a49893
den3yb <den3yb@gmail.com>
2025-04-23 14:42:52 +0400

    переделанна для работы с OpenMP

--- graph.py
+++ graph.py
@@ -1,32 +1,81 @@
 import matplotlib.pyplot as plt
 
-def plot_from_file(filename):
+def average_plot_from_files(filenames, label):
     """
-    Строит график по файлу со статистикой
+    Строит график по нескольким файлам со статистикой, усредняя значения и добавляет метку.
     """
-    x = []
-    y = []
-    with open(filename, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if line:
-                values = line.split()
-                x_value = float(values[0])
-                y_value = float(values[1])
-                x.append(x_value)
-                y.append(y_value)
+    all_x = []
+    all_y = []
 
+    # Читаем данные из всех файлов
+    for filename in filenames:
+        x = []
+        y = []
+        with open(filename, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    values = line.split()
+                    x_value = float(values[0])
+                    y_value = float(values[1])
+                    x.append(x_value)
+                    y.append(y_value)
+        all_x.append(x)
+        all_y.append(y)
 
-    if x and y:
-        plt.plot(x, y, marker='o')  
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.grid(True)
-        plt.show()
-    else:
-        print("No data to plot.")
+    # Находим уникальные значения x, чтобы по ним усреднять
+    unique_x = sorted(list(set().union(*all_x)))
 
+    # Усредняем значения y для каждого x
+    average_y = []
+    for x_val in unique_x:
+        y_values_for_x = []
+        for i in range(len(filenames)):
+            if len(all_x[i]) == 0: # Проверяем, есть ли данные в all_x[i]
+                continue # Если нет, переходим к следующему файлу
+
+            try:
+                index = all_x[i].index(x_val)
+                y_values_for_x.append(all_y[i][index])
+            except ValueError:
+                pass # x_val отсутствует в текущем файле
+
+        if y_values_for_x:
+            average_y.append(sum(y_values_for_x) / len(y_values_for_x))
+        else:
+            average_y.append(None)
+
+    # Убираем значения None, чтобы matplotlib не ругался
+    x_filtered = []
+    y_filtered = []
+    for i in range(len(unique_x)):
+        if average_y[i] is not None:
+            x_filtered.append(unique_x[i])
+            y_filtered.append(average_y[i])
+
+    return x_filtered, y_filtered, label
 
 if __name__ == '__main__':
-    filename = 'matrix_out\\matrix_statistic.txt'  
-    plot_from_file(filename)
+    plt.figure(figsize=(10, 6))  # Увеличиваем размер графика для лучшей читаемости
+
+    # Данные для 8 потоков
+    filenames_8 = [f'Parallel-prog\\matrix_out\\matrix_statistic8{i}.txt' for i in range(1, 6)]
+    x_8, y_8, label_8 = average_plot_from_files(filenames_8, '8 потоков')
+    plt.plot(x_8, y_8, marker='o', label=label_8)
+
+    # Данные для 4 потоков
+    filenames_4 = [f'Parallel-prog\\matrix_out\\matrix_statistic4{i}.txt' for i in range(1, 6)]
+    x_4, y_4, label_4 = average_plot_from_files(filenames_4, '4 потока')
+    plt.plot(x_4, y_4, marker='o', label=label_4)
+
+    # Данные для 2 потоков
+    filenames_2 = [f'Parallel-prog\\matrix_out\\matrix_statistic2{i}.txt' for i in range(1, 6)]
+    x_2, y_2, label_2 = average_plot_from_files(filenames_2, '2 потока')
+    plt.plot(x_2, y_2, marker='o', label=label_2)
+
+    plt.xlabel('Количество элементов матрицы')
+    plt.ylabel('Среднее время выполнения (секунды)')
+    plt.title('Усредненное время выполнения умножения матриц для разного количества потоков')
+    plt.grid(True)
+    plt.legend()  # Добавляем легенду, чтобы различать графики
+    plt.show()
--- main.cpp
+++ main.cpp
@@ -1,12 +1,106 @@
-#include "alg.h"
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include <omp.h>
 
 using namespace std;
+
+
+namespace constants {
+    string first_file = "matrix_in\\first_matrix.txt";
+    string second_file = "matrix_in\\second_matrix.txt";
+    string result_file = "matrix_out\\result_matrix.txt";
+    string statistic_file = "matrix_out\\matrix_statistic.txt";
+}
+
+namespace algoritms {
+
+    bool createRandomMatrixFile(int row, int colum, string file_name) {
+        // Функция создаёт файл с матрицой рандомного содержания, по заданным размерам
+        srand(time(0));
+        ofstream file(file_name);
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < colum; ++j) { file << rand() << " "; }
+            file << endl;
+        }
+        file.close();
+        return true;
+    }
+
+    std::vector<std::vector<int>> readMatrixFromFile(int row, int colum, string file_name) {
+        // Функция считывает из файла матрицу рандомного содержания, по заданным размерам
+        std::vector<std::vector<int>> matrix(row, std::vector<int>(colum));
+        std::ifstream file(file_name);
+        int number;
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < colum; ++j) {
+                if (file >> number) {
+                    matrix[i][j] = number;
+                } else {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        file.close();
+        return matrix;
+    }
+
+    vector<vector<int>> multiplySaveMatrixes(vector<vector<int>>& matrix1, vector<vector<int>>& matrix2) {
+        // выполняет произведение матриц и записывает результат в файл
+        if (matrix1[0].size() != matrix2.size()) {
+            cerr << "Матрицы не могут быть умноженны" << endl;
+            return {};
+        }
+
+        int row = matrix1.size();
+        int colum = matrix2[0].size();
+
+        vector<vector<int>> result(row, vector<int>(colum, 0));
+
+        // Распараллеливаем внешний цикл по строкам результирующей матрицы
+        omp_set_num_threads(2);
+        #pragma omp parallel for schedule(auto) 
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < colum; ++j) {
+                for (int k = 0; k < matrix1[0].size(); ++k) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        ofstream file(constants::result_file);
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < colum; ++j) {
+                file << result[i][j] << " ";
+            }
+            file << endl;
+        }
+        file.close();
+
+        return result;
+    }
+
+    void printMatrix(const vector<vector<int>>& matrix) {
+        // функция вывода матрицы в консоль
+        for (const auto& row : matrix) {
+            for (int val : row) {
+                cout << val << " ";
+            }
+            cout << endl;
+        }
+    }
+
+}
+
 using namespace algoritms;
 
 void oneCycle(int row_1, int colum_1, int row_2, int colum_2){
     //Функция делает прогон программы один раз, создавая рандомные значения для обеих матриц
     // и перемножая их
+
 
     createRandomMatrixFile(row_1, colum_1, constants::first_file);
     vector<vector<int>> matrix_1 = readMatrixFromFile(row_1, colum_1, constants::first_file);
